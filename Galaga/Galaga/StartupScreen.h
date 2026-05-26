d312c72effe931241b2280f71a52f72c7faf0f45
sera144 <serasera@inha.edu>
2026-05-26 23:38:08 +0900

    Review Plz: Falling Stars 수정, Start 기능 수정, Restart 기능 수정

--- Galaga/Galaga/StartupScreen.h
+++ Galaga/Galaga/StartupScreen.h
@@ -4,9 +4,15 @@
 class StartupScreen
 {
 public:
-    StartupScreen() = default;
-
     void Initialize();
-
+    void Input();
     void Render(GraphicsContext& graphics, float globalTime);
+
+    bool ShouldStartGame()   const { return StartPressed; }
+    bool ShouldRestartGame() const { return RestartPressed; }
+    void Reset();
+
+private:
+    bool StartPressed = false;
+    bool RestartPressed = false;
 };
