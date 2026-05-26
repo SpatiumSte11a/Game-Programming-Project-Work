e5e6c1c40eae2d6b2876a4ea80a102dd17e5454e
sera144 <serasera@inha.edu>
2026-05-26 19:32:28 +0900

    StartupScreen, GlobalTime, PlayerLives, RenderLives  구현

--- Galaga/Galaga/Game.h
+++ Galaga/Galaga/Game.h
@@ -7,18 +7,33 @@
 #include "Player.h"
 #include "Bullet.h"
 #include "Enemy.h"
+#include "StartupScreen.h"
+
+enum class GameState
+{
+    Startup, 
+    Playing, 
+    Paused, 
+    Exit
+};
 
 class Game
 {
 private:
     WindowContext Window;
     GraphicsContext Graphics;
+    StartupScreen StartScreen; 
+
     bool IsRunning;
+    GameState CurrentState; 
 
     std::chrono::high_resolution_clock::time_point PrevTime;
 
     float TitleUpdateTimer;
     int FrameCounter;
+    
+    float GlobalTime; 
+    int PlayerLives; 
 
     Player PlayerObject;
     PlayerBulletSystem PlayerBulletSystemObject;
@@ -36,7 +51,9 @@ public:
 private:
     float GetDeltaTime();
 
+    void ResetGame(); 
     void Input();
     void Update(float dt);
     void Render();
+    void RenderLives(); 
 };
